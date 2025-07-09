0e1ba2cd8cc1de8e0b934b41318efd39e276aa6e
whm <q729969151@163    .com>
2025-07-09 18:02:53 +0800

    20250709

--- docs/utils/performance/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-current_datetime=$(date +%Y%m%d%H%M%S)
-mkdir -p "${current_datetime}"
-# touch "${current_datetime}/output.jtl"  # 无需先手动创建
-mkdir -p "${current_datetime}/results"
-jmeter -n -t 202409.jmx -l "${current_datetime}/output.jtl" -e -o "${current_datetime}/results"
